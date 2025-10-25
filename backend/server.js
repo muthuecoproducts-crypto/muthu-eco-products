06a3654ae83fcecf9ffa5bff21e24377976dcad6
DharanSJIT <dharan.mj2k@gmail.com>
2025-10-25 18:18:42 +0530

    updated version

--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,16 @@ app.get('/', (req, res) => {
 
 // Create transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  debug: true,
-  logger: true
+  tls: {
+    rejectUnauthorized: false
+  }
 })
 
 // Contact form endpoint
